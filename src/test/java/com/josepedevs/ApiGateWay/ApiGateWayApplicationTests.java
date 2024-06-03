822627600078504d3867008674600b5d9f2bc109
Jose Pedro <jose.navarro.vicente@accenture.com>
2024-06-03 13:48:02 +0200

    Prepared API Gateway for AuthService and ClientDataService

---
+++ src/test/java/com/josepedevs/ApiGateWay/ApiGateWayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josepedevs.ApiGateWay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGateWayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
